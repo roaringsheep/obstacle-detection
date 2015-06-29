0cd4241ee0864717c6054828717b964060d69b61
Mark Fox <mf@ixel.org>
2015-06-29 15:19:46 -0700

    stubbed an initial spec spec

---
+++ specs/obstacle-deteciton.spec.js
@@ -0,0 +1,34 @@
+
+describe('obstacle detection h.u.d.' function() {
+    it("should resize to it's container", function() {
+
+    });
+
+    it("should pan viewport with mouse click and drag", function() {
+
+    });
+
+    it("should pan viewport with touch drag", function() {
+
+    });
+
+    it("should zoom viewport with mouse scroll-wheel", function() {
+
+    });
+
+    it("should zoom viewport with touch pinch", function() {
+
+    });
+
+    it("should position obstacles accurately", function() {
+
+    });
+
+    it("should indicate obstacles out of view but in range", function() {
+
+    });
+
+    it("should position the drone's location accurately", function() {
+        
+    });
+});
